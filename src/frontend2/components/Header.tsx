a97eea152139e4a72aba8ba233ae559fdba53c70
Anirudh <anisur@cs.washington.edu>
2024-02-20 12:17:49 -0800

    add second version with auth
    update gitignore

---
+++ src/frontend2/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="flex flex-col gap-16 items-center">
+      <div className="flex gap-8 justify-center items-center">
+      </div>
+      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+        KAAA 🦅🦅🦅{" "}
+      </p>
+      <p className="text-xl italic"> Grow closer to friends with Periodic Drawing Prompts </p>
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
+  );
+}
