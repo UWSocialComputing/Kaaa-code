a04751c006a3e5f25e32d0c5bdd315b689344c71
Anirudh <anisur@cs.washington.edu>
2024-02-25 19:32:14 -0800

    minor bug fix

--- src/frontend2/app/page.tsx
+++ src/frontend2/app/page.tsx
@@ -1,22 +1,19 @@
-import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  const supabase = createClient();
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect("/dashboard");
+  }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
