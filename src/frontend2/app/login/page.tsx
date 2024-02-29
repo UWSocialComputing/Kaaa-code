5992c91b150087867714c9ce8a32d3e0beef16cd
Asa Grekin[user] <asagrekin@gmail.com>
2024-02-28 18:09:01 -0800

    Moved sign up to its own page

--- src/frontend2/app/login/page.tsx
+++ src/frontend2/app/login/page.tsx
@@ -35,31 +35,6 @@ export default function Login({
     return redirect("/dashboard");
   };
 
-  // Query backend to create new user
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      console.log(error);
-      return redirect("/login?message="+error.message);
-    }
-
-    return redirect("/login?message=Please sign in with your new account");
-  };
-
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
@@ -110,13 +85,9 @@ export default function Login({
         >
           Sign In
         </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
+        <Link href="/signup" className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 text-center">
           Sign Up
-        </SubmitButton>
+        </Link>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
             {searchParams.message}
